0887ca775097a1b5c715fb35956d6b92dd83ab5f
AlbertoSantos19 <albertosantos.hrzon@gmail.com>
2020-06-06 20:56:00 -0300

    Salvando nome dos jogadores

---
+++ main/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string nomeJogador1, nomeJogador2;
+
+    char tabuleiro[12][12];
+
+    cout<<"Bem vindo ao jogo Batalhas da Lapônia"<<endl<<endl;
+    cout<<"Por favor, insira o nome do jogador 1: ";
+    cin>>nomeJogador1;
+    cout<<"Por favor, insira o nome do jogador 2: ";
+    cin>>nomeJogador2;
+    
+    
+
+
+}
