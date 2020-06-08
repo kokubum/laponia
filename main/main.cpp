e6a37bc23007f4f8d308c72835aba5708951bbf1
AlbertoSantos19 <albertosantos.hrzon@gmail.com>
2020-06-07 21:07:32 -0300

    criando funcao para sortear o turno dos jogadores

--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <random>
 
 using namespace std;
 
@@ -31,6 +32,31 @@ void printarTabuleiro(char tabuleiro[12][12] ){
     cout<<endl;
 }
 
+bool sortearJogador(int numeros[], int qtdNumeros, int limite){
+    int qtd0 = 0;
+    int qtd1 = 0;
+
+    srand(time(NULL));
+
+    for(int i=0;i<qtdNumeros;i++){
+        numeros[i] = rand() % limite;
+    }
+
+    for(int i=0;i<qtdNumeros;i++){
+        if(numeros[i] == 0){
+            qtd0++;
+        } else {
+            qtd1++;
+        }
+    }
+
+    if(qtd0 > qtd1){
+        return true; //Jogador 1 começa
+    } else {
+        return false; //Jogador 2 começa
+    }
+}
+
 int main() {
 
     string nomeJogador1, nomeJogador2;
@@ -44,7 +70,23 @@ int main() {
     cin>>nomeJogador2;
 
     criarTabuleiro(tabuleiro);
+
+    cout<<"Jogadores registrados!"<<endl;
+    cout<<"Preenchimento do tabuleiro: será sorteado o primeiro jogador a colocar sua peça"<<endl;
+    cout<<"e será feita de forma alternada até que os 8 guerreiros de cada lado sejam colocados."<<endl;
+    cout<<"O formato de preenchimento segue o padrao x,x onde o primeiro x é a linha em que deve ser"<<endl;
+    cout<<"colocada a peça e o outro x a coluna, caso o jogador coloque em uma posição inválida será passada a vez"<<endl;
+    cout<<"a cada turno do posicionamento o tabuleiro será exibido, boa sorte."<<endl<<endl;
     printarTabuleiro(tabuleiro);
 
+    bool turno; //true para jogador 1, else para jogador 2
 
+    int numeros[501];
+    turno = sortearJogador(numeros, 501, 2);
+
+    if(turno){
+        cout<<nomeJogador1<<" "<<"começa."<<endl;
+    } else {
+        cout<<nomeJogador2<<" "<<"começa."<<endl;
+    }
 }
