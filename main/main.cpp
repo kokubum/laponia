59ac38cd5e77af7a3e2968aa3bd8a569f2fc182e
Kokubum <eskokubum@gmail.com>
2020-06-09 15:00:09 -0300

    Laponia Completo

--- main/main.cpp
+++ main/main.cpp
@@ -1,135 +1,327 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
+#define TAM 12
+#define QTD_G 1
 
 using namespace std;
 
-void criarTabuleiro(char tabuleiro[12][12] ){
-    for(int i=0;i<12;i++){
-        for(int j=0;j<12;j++){
-            if(i == 0 || i == 11){
-                tabuleiro[i][j] = '*';
-            } else if(i == 5 || i == 6){
-                tabuleiro[i][j] = '~';
-            } else if(j == 0 || j == 11){
-                tabuleiro[i][j] = '*';
-            } else {
-                tabuleiro[i][j] = ' ';
-            }
-        }
-    }
+int somadorX(int posicaoX){
+  if (posicaoX<5){
+    return 1;
+  }
+  return -1;
 }
 
-void printarTabuleiro(char tabuleiro[12][12] ){
-    for(int i=0;i<12;i++){
-        for(int j=0;j<12;j++){
-            cout<<tabuleiro[i][j];
-            if(j == 11){
-                cout<<endl;
-            }
-        }
+int somadorY(int direcaoAtq,int posicaoX){
+  if (((direcaoAtq == 1) && (posicaoX<5)) || ((direcaoAtq==2)&&(posicaoX>6))){
+    return 1;
+  }
+  return -1;
+}
+
+char numeroGuerreiro(string jogador,string nomeJogador1,string nomeJogador2){
+  if (jogador == nomeJogador1){
+    return '1';
+  }
+  return '2';
+}
+
+
+char guerreirOposto(char guerreiro){
+  if (guerreiro =='1'){
+    return '2';
+  }
+  return '1';
+}
+
+void criarTabuleiro(char tabuleiro[][TAM]){
+  for (int i=0;i<TAM;i++){
+    for (int j=0;j<TAM;j++){
+      if (i==0 || j==0 || i==(TAM-1) || j==(TAM-1)){
+        tabuleiro[i][j] = '*';
+      }
+      else if (i==5 || i==6){
+        tabuleiro[i][j] = '~';
+      }
+      else{
+        tabuleiro[i][j] = ' ';
+      }
+    }
+  }
+}
+
+
+void printarTabuleiro(char tabuleiro[][12]){
+
+  for (int i=0;i<TAM;i++){
+    cout<<"\t\t"<<setw(2)<<i<<"  ";
+    for (int j=0;j<TAM;j++){
+      cout<<setw(3)<<tabuleiro[i][j]<<' ';
+    }
+    cout<<endl<<endl;
+  }
+  cout<<"\t\t   ";
+  for (int i=0;i<TAM;i++){
+    cout<<setw(4)<<i;
+  }
+  cout<<endl;
+}
+
+string sortearJogador(string jogador1,string jogador2){
+  srand(time(NULL));
+
+  if ((rand()%2 +1)==2){
+    return jogador2;
+  }
+  return jogador1;
+
+}
+
+
+
+string mudaVez(string jogador,string nomeJogador1,string nomeJogador2){
+  if (jogador == nomeJogador1){
+    return nomeJogador2;
+  }
+  return nomeJogador1;
+}
+
+bool validarPosicao(int posicaoX,int posicaoY,char tabuleiro[][12],char guerreiro=' '){
+
+  if (posicaoX>0 && posicaoX<TAM && posicaoY>0 && posicaoY<TAM){
+    if (tabuleiro[posicaoX][posicaoY]==guerreiro){
+      return true;
+    }
+  }
+  return false;
+
+}
+
+
+
+void preencherTabuleiro(string jogador,string nomeJogador1,string nomeJogador2,char tabuleiro[][12]){
+  int qtd = 0;
+  int posicaoX,posicaoY;
+  char virgula;
+
+  while(qtd!=QTD_G*2){
+    cout<<"Rodada: "<<jogador<<endl;
+    cout<<"Posição do seu guerreiro (X,Y): ";
+    cin>>posicaoX>>virgula>>posicaoY;
+    cout<<endl;
+    if (validarPosicao(posicaoX,posicaoY,tabuleiro)){
+      tabuleiro[posicaoX][posicaoY] = numeroGuerreiro(jogador,nomeJogador1,nomeJogador2);
+      jogador = mudaVez(jogador,nomeJogador1,nomeJogador2);
+      qtd++;
+      printarTabuleiro(tabuleiro);
+    }
+    else {
+      cout<<"--------------------------------------------------------------------------------------"<<endl;
+      if (tabuleiro[posicaoX][posicaoY]=='1' || tabuleiro[posicaoX][posicaoY] == '2'){
+        cout<<"--> Posicao Ocupada"<<endl;
+      }
+      else{
+        cout<<"--> Posicao Invalida"<<endl;
+      }
     }
     cout<<endl;
+
+  }
+
+
 }
 
-bool sortearJogador(int numeros[], int qtdNumeros, int limite){
-    int qtd0 = 0;
-    int qtd1 = 0;
-
-    srand(time(NULL));
-
-    for(int i=0;i<qtdNumeros;i++){
-        numeros[i] = rand() % limite;
-    }
-
-    for(int i=0;i<qtdNumeros;i++){
-        if(numeros[i] == 0){
-            qtd0++;
-        } else {
-            qtd1++;
-        }
-    }
-
-    if(qtd0 > qtd1){
-        return true; //Jogador 1 começa
-    } else {
-        return false; //Jogador 2 começa
-    }
+bool movimento(int origemX,int origemY,int destinoX,int destinoY,char tabuleiro[][12]){
+  if ((abs(origemX-destinoX)>=1 && abs(origemY-destinoY)>=1) || tabuleiro[destinoX][destinoY]!=' '){
+    return false;
+  }
+  return true;
 }
 
-void preencherTabuleiro(bool turno, string nomeJogador1, string nomeJogador2, char tabuleiro[12][12]){
-    int qtdPecasJogador1 = 8;
-    int qtdPecasJogador2 = 8;
-    while(qtdPecasJogador1 != 0 || qtdPecasJogador2 != 0){
-        int linha,coluna;
-        char virgula;
-
-        if(turno){
-            cout<<nomeJogador1<<", insira a posição do tabuleiro (ex: x,x): ";
-        } else {
-            cout<<nomeJogador2<<", insira a posição do tabuleiro (ex: x,x): ";
-        }
-        cin>>linha>>virgula>>coluna;
-
-        if(linha <= 0 || linha == 5 || linha == 6 || linha >= 11 || coluna <= 0 || coluna >= 11){
-            cout<<"Valor digitado inválido, você perdeu a vez."<<endl;
-        } else {
-            if(tabuleiro[linha][coluna] != ' '){
-                cout<<"Nesta posição já existe uma peça, você perdeu a vez."<<endl;
-            }else {
-                if(turno){
-                    tabuleiro[linha][coluna] = '1';
-                    qtdPecasJogador1--;
-                } else {
-                    tabuleiro[linha][coluna] = '2';
-                    qtdPecasJogador2--;
-                }
-            }
-        }
-
-        if(turno){
-            turno = false;
-        } else {
-            turno = true;
-        }
-        printarTabuleiro(tabuleiro);
+bool ataqueDiagonal(int contY,int contX,int posicaoAtqX,int posicaoAtqY,char guerreiro,char tabuleiro[][12]){
+  char inimigo = guerreirOposto(guerreiro);
+  bool interrompido = false;
+  cout<<"--------------------------------------------------------------------------------------"<<endl;
+  while(posicaoAtqX<(TAM-1) && posicaoAtqX>0 && posicaoAtqY<(TAM-1) && !interrompido){
+    if(tabuleiro[posicaoAtqX][posicaoAtqY]==inimigo){
+      tabuleiro[posicaoAtqX][posicaoAtqY] = ' ';
+      cout<<"--> Guerreiro Da Posicao "<<posicaoAtqX<<","<<posicaoAtqY<<" Morreu!"<<endl;
+      return true;
     }
+    if(tabuleiro[posicaoAtqX][posicaoAtqY]==guerreiro){
+      cout<<"--> Guerreiro Aliado No Caminho"<<endl;
+      interrompido = true;
+    }
+    posicaoAtqX+=contX;
+    posicaoAtqY+=contY;
+  }
+
+  return false;
 }
 
-int main() {
+bool ataqueFrontal(int contX,int posicaoAtqX,int posicaoY,char guerreiro,char tabuleiro[][12]){
+  char inimigo = guerreirOposto(guerreiro);
+  bool interrompido = false;
+  cout<<"--------------------------------------------------------------------------------------"<<endl;
+  while (posicaoAtqX<(TAM-1) && posicaoAtqX>0 && !interrompido){
+    if (tabuleiro[posicaoAtqX][posicaoY]==inimigo){
+      tabuleiro[posicaoAtqX][posicaoY] = ' ';
+      cout<<"--> Guerreiro Da Posicao "<<posicaoAtqX<<","<<posicaoY<<" Morreu!"<<endl;
+      return true;
 
-    string nomeJogador1, nomeJogador2;
+    }
+    if(tabuleiro[posicaoAtqX][posicaoY]==guerreiro){
+      cout<<"--> Guerreiro Aliado No Caminho"<<endl;
+      interrompido = true;
+    }
+    posicaoAtqX+=contX;
+  }
 
-    char tabuleiro[12][12];
+  return false;
+}
 
-    cout<<"Bem vindo ao jogo Batalhas da Lapônia"<<endl<<endl;
-    cout<<"Por favor, insira o nome do jogador 1: ";
-    cin>>nomeJogador1;
-    cout<<"Por favor, insira o nome do jogador 2: ";
-    cin>>nomeJogador2;
+void ataque(int posicaoX,int posicaoY,char tabuleiro[][12],char guerreiro,int qtd_jogador[]){
 
-    criarTabuleiro(tabuleiro);
+  int contX=somadorX(posicaoX);
+  int posicaoAtqX;
+  bool atq;
 
-    cout<<"Jogadores registrados!"<<endl;
-    cout<<"Preenchimento do tabuleiro: será sorteado o primeiro jogador a colocar sua peça"<<endl;
-    cout<<"e será feita de forma alternada até que os 8 guerreiros de cada lado sejam colocados."<<endl;
-    cout<<"O formato de preenchimento segue o padrao x,x onde o primeiro x é a linha em que deve ser"<<endl;
-    cout<<"colocada a peça e o outro x a coluna, caso o jogador coloque em uma posição inválida será passada a vez"<<endl;
-    cout<<"a cada turno do posicionamento o tabuleiro será exibido, boa sorte."<<endl<<endl;
+  posicaoAtqX = posicaoX+contX;
+  if (guerreiro == '1'){
+    atq = ataqueFrontal(contX,posicaoAtqX,posicaoY,guerreiro,tabuleiro);
+  }
+  else{
+    int direcaoAtq;
+    cout<<"Ataque Diagonal (Esquerda: 1 | Direita: 2): ";
+    cin>>direcaoAtq;
+    int contY = somadorY(direcaoAtq,posicaoX);
+    int posicaoAtqY = posicaoY+contY;
+    atq = ataqueDiagonal(contY,contX,posicaoAtqX,posicaoAtqY,guerreiro,tabuleiro);
+  }
+  if (atq){
+    cout<<"--> Ataque Realizado com Sucesso!"<<endl;
+    qtd_jogador[(int)(guerreirOposto(guerreiro)) - '0' - 1]--;
+  }
+  else{
+    cout<<"--> Ataque Falhou!"<<endl;
+  }
+
+}
+
+
+void realizarJogada(string &jogador,string nomeJogador1,string nomeJogador2,char tabuleiro[][12],int qtd_jogador[]){
+  int posicaoX,posicaoY,jogada,destinoX,destinoY;
+  char guerreiro = numeroGuerreiro(jogador,nomeJogador1,nomeJogador2);
+  char virgula;
+
+  cout<<"Rodada: "<<jogador<<endl;
+  cout<<"Escolha seu guerreiro (X,Y): ";
+  cin>>posicaoX>>virgula>>posicaoY;
+  if (validarPosicao(posicaoX,posicaoY,tabuleiro,guerreiro)){
+    cout<<"Escolher jogada (movimento: 1 | ataque: 2): ";
+    cin>>jogada;
+    if (jogada==1){
+      cout<<"Posicao que deseja ir (X,Y): ";
+      cin>>destinoX>>virgula>>destinoY;
+      cout<<"--------------------------------------------------------------------------------------"<<endl;
+      if (movimento(posicaoX,posicaoY,destinoX,destinoY,tabuleiro)){
+        tabuleiro[destinoX][destinoY]=tabuleiro[posicaoX][posicaoY];
+        tabuleiro[posicaoX][posicaoY]=' ';
+        cout<<"---> Guerreiro "<<guerreiro<<": ("<<posicaoX<<","<<posicaoY<<") ->"<<" ("<<destinoX<<","<<destinoY<<")\n"<<endl;
+        jogador = mudaVez(jogador,nomeJogador1,nomeJogador2);
+      }
+      else{
+        cout<<"---> Posicao Invalida - Tente Novamente"<<endl;
+      }
+    }
+    else if (jogada==2){
+      ataque(posicaoX,posicaoY,tabuleiro,guerreiro,qtd_jogador);
+      jogador = mudaVez(jogador,nomeJogador1,nomeJogador2);
+    }
+  }
+  else{
+    cout<<"--------------------------------------------------------------------------------------"<<endl;
+    cout<<"---> Guerreiro Invalido - Tente Novamente"<<endl;
+  }
+
+}
+
+void laponiaGame(char tabuleiro[][12],string nomeJogador1,string nomeJogador2,string jogador){
+  int qtd_jogador[]={QTD_G,QTD_G};
+  int posicaoX,posicaoY;
+  char virgula;
+  while (qtd_jogador[0]!=0 && qtd_jogador[1]!=0){
+
     printarTabuleiro(tabuleiro);
+    realizarJogada(jogador,nomeJogador1,nomeJogador2,tabuleiro,qtd_jogador);
+  }
+  cout<<"--------------------------------------Fim de Jogo---------------------------------------"<<endl;
+  cout<<"GANHADOR: ";
+  if (qtd_jogador[0]==0){
+    cout<<nomeJogador2<<endl;
+  }
+  else{
+    cout<<nomeJogador2<<endl;
+  }
 
-    bool turno; //true para jogador 1, else para jogador 2
 
-    int numeros[501];
-    turno = sortearJogador(numeros, 501, 2);
+}
+//
+// bool sortearJogador(int numeros[], int qtdNumeros, int limite){
+//     int qtd0 = 0;
+//     int qtd1 = 0;
+//
+//     srand(time(NULL));
+//
+//     for(int i=0;i<qtdNumeros;i++){
+//         numeros[i] = rand() % limite;
+//     }
+//
+//     for(int i=0;i<qtdNumeros;i++){
+//         if(numeros[i] == 0){
+//             qtd0++;
+//         } else {
+//             qtd1++;
+//         }
+//     }
+//
+//     if(qtd0 > qtd1){
+//         return true; //Jogador 1 começa
+//     } else {
+//         return false; //Jogador 2 começa
+//     }
+// }
 
-    if(turno){
-        cout<<nomeJogador1<<" "<<"começa."<<endl;
-    } else {
-        cout<<nomeJogador2<<" "<<"começa."<<endl;
-    }
 
-    preencherTabuleiro(turno, nomeJogador1, nomeJogador2, tabuleiro);
 
-    cout<<"Todas peças colocadas!"<<endl;
-    cout<<"Começa o jogo: "<<endl;
+
+
+int main(){
+  string nomeJogador1,nomeJogador2,jogador;
+  int start;
+  char tabuleiro[TAM][TAM];
+  criarTabuleiro(tabuleiro);
+  cout<<"---------------------------------Batalhas da Lapônia------------------------------------"<<endl<<endl;
+  cout<<"Jogador 1: ";
+  cin>>nomeJogador1;
+  cout<<"Jogador 2: ";
+  cin>>nomeJogador2;
+  cout<<endl;
+  jogador = sortearJogador(nomeJogador1,nomeJogador2);
+  cout<<"Sorteio realizado: "<<jogador<<endl;
+  cout<<"-------------------------------Preenchendo o Tabuleiro----------------------------------"<<endl<<endl;
+  printarTabuleiro(tabuleiro);
+  preencherTabuleiro(jogador,nomeJogador1,nomeJogador2,tabuleiro);
+  cout<<"----------------------------------------------------------------------------------------"<<endl<<endl;
+  cout<<"Iniciar (start: 1 | exit: 0): ";
+  cin>>start;
+  cout<<endl;
+  if (start){
+    laponiaGame(tabuleiro,nomeJogador1,nomeJogador2,jogador);
+  }
+  cout<<endl;
+
+
+
 }
